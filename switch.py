cdb9a6447b8a66edb51a56c2c3c0bc1e62520aad
Koen Hendriks <info@koenhendriks.nl>
2023-12-23 22:29:57 +0100

    Removed unused code from Switch entities.

--- switch.py
+++ switch.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import logging
 
-from homeassistant.components.switch import (SwitchEntity, PLATFORM_SCHEMA, SwitchDeviceClass)
+from homeassistant.components.switch import (SwitchEntity, SwitchDeviceClass)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
