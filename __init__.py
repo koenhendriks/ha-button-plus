8760e076c063617dac6452732bbeeb02a9ec4205
Koen Hendriks <info@koenhendriks.nl>
2023-12-23 22:48:24 +0100

    Add light platform to be loaded

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ _LOGGER = logging.getLogger(__name__)
 
 # List of platforms to support. There should be a matching .py file for each,
 # eg <cover.py> and <sensor.py>
-PLATFORMS: list[str] = ["switch"]
+PLATFORMS: list[str] = ["switch", "light"]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
