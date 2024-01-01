7a70ccf5e920e27eecb5c5f7c1b3a046762c6169
Koen Hendriks <info@koenhendriks.nl>
2024-01-01 20:09:04 +0100

    Add button entity with data coordinator for MQTT subscriptions.

---
+++ button.py
@@ -0,0 +1,104 @@
+""" Platform for button integration. """
+from __future__ import annotations
+
+import logging
+
+from homeassistant.components.button import ButtonEntity, ButtonDeviceClass
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from . import ButtonPlusHub
+
+from .const import DOMAIN, MANUFACTURER
+from .coordinator import ButtonPlusCoordinator
+
+_LOGGER = logging.getLogger(__name__)
+
+button_entities = []
+
+
+async def async_setup_entry(
+        hass: HomeAssistant,
+        config_entry: ConfigEntry,
+        async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Add button_entities for passed config_entry in HA."""
+
+    hub: ButtonPlusHub = hass.data[DOMAIN][config_entry.entry_id]
+    coordinator = ButtonPlusCoordinator(hass, hub)
+
+    await coordinator.async_config_entry_first_refresh()
+
+    buttons = hub.config.mqtt_buttons
+
+    for button in buttons:
+        _LOGGER.debug(f"Creating button with parameters: {button.button_id} {button.label} {hub.hub_id}")
+        entity = ButtonPlusButton(coordinator, button.button_id, hub)
+        button_entities.append(entity)
+        hub.add_button(button.button_id, entity)
+
+    async_add_entities(button_entities)
+
+
+class ButtonPlusButton(CoordinatorEntity, ButtonEntity):
+    def __init__(self, coordinator, btn_id: int, hub: ButtonPlusHub):
+        self._is_on = False
+        self._hub_id = hub.hub_id
+        self._hub = hub
+        self._btn_id = btn_id
+        self._attr_unique_id = f'button-{self._hub_id}-{btn_id}'
+        self.entity_id = f"button.{self._hub_id}_{btn_id}"
+        self._attr_name = f'button-{btn_id}'
+        self._name = f'Button {btn_id}'
+        self._device_class = ButtonDeviceClass.IDENTIFY
+
+        super().__init__(coordinator, context={btn_id})
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        _LOGGER.debug(f"Coordinator update for {self._btn_id}")
+
+    @property
+    def name(self) -> str:
+        """Return the display name of this button."""
+        return self._name
+
+    @property
+    def should_poll(self) -> bool:
+        """Return the display name of this button."""
+        return False
+
+    @property
+    def device_info(self):
+        """Return information to link this entity with the correct device."""
+        device_info = {
+            "via_device": (DOMAIN, self._hub.hub_id),
+            "manufacturer": MANUFACTURER,
+        }
+
+        match self._btn_id:
+            case 0 | 1:
+                return {"identifiers": {(DOMAIN, self._hub_id)}}
+            case 2 | 3:
+                device_info["name"] = f"BAR Module 1"
+                device_info["connections"] = {("bar_module", 1)}
+                device_info["model"] = "BAR Module"
+                device_info["identifiers"] = {(DOMAIN, f'{self._btn_id}_bar_module_1')}
+            case 4 | 5:
+                device_info["name"] = f"BAR Module 2"
+                device_info["connections"] = {("bar_module", 2)}
+                device_info["model"] = "BAR Module"
+                device_info["identifiers"] = {(DOMAIN, f'{self._btn_id}_bar_module_2')}
+            case 6 | 7:
+                device_info["name"] = f"BAR Module 3"
+                device_info["connections"] = {("bar_module", 3)}
+                device_info["model"] = "BAR Module"
+                device_info["identifiers"] = {(DOMAIN, f'{self._btn_id}_bar_module_3')}
+
+        return device_info
+
+    async def async_press(self, from_mqtt=False) -> None:
+        """Handle the button press."""
+        _LOGGER.debug(f"async press from mqtt: {from_mqtt}")
--- coordinator.py
+++ coordinator.py
@@ -1,11 +1,55 @@
+from homeassistant.components.button import ButtonEntity
+from . import DOMAIN, ButtonPlusHub
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.components.mqtt import client as mqtt, ReceiveMessage
+
+import logging
+import re
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class ButtonPlusCoordinator(DataUpdateCoordinator):
     """Button Plus coordinator."""
 
-    async def _async_update_data(self):
-        """Fetch data from MQTT subscriptions
-        """
+    def __init__(self, hass: HomeAssistant, hub: ButtonPlusHub):
+        """Initialize my coordinator."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            name=f"{DOMAIN}_coordinator",
+            update_interval=None,
+            update_method=None,
+        )
+        self.hub = hub
+        self._hass = hass
+        self._mqtt_subscribed_buttons = False
+        self._mqtt_topic_buttons = "buttonplus/+/button/+/click"
 
+    async def _async_update_data(self):
+        """Create MQTT subscriptions for buttonplus """
+        _LOGGER.debug(f"Initial data fetch from coordinator")
+        if not self._mqtt_subscribed_buttons:
+            self.unsubscribe_mqtt = await mqtt.async_subscribe(
+                self._hass,
+                self._mqtt_topic_buttons,
+                self.mqtt_button_callback,
+                0
+            )
+            _LOGGER.debug(f"MQTT subscribed to {self._mqtt_topic_buttons}")
+
+    async def mqtt_button_callback(self, message: ReceiveMessage):
+        # Handle the message here
+        _LOGGER.debug(f"Received message on topic {message.topic}: {message.payload}")
+        match = re.search(r'/(\d+)/click', message.topic)
+        btn_id = int(match.group(1)) if match else None
+
+        entity: ButtonEntity = self.hub.button_entities[str(btn_id)]
+
+        await self.hass.services.async_call(
+            "button",
+            'press',
+            target={"entity_id": entity.entity_id}
+        )
 
